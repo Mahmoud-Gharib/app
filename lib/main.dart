d37e33da2e37de53e4f07269b02cf47f28b4e4c6
Mahmoud Gharib <62407045+Mahmoud-Gharib@users.noreply.github.com>
2025-07-10 16:43:50 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -32,8 +32,8 @@ class _UploadImagePageState extends State<UploadImagePage> {
   String _status = '';
 
   // ✅ التوكن هنا مباشرة
-  final String githubToken = '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'; // ✋ استبدل ده بتوكنك
-  final String repoOwner = 'mahmoud-gharib';
+  final String githubToken = 'ithub_pat_11AO4EDBI0UeVuuSxfuiQO_VqY08TaJ5m3CxwTsPRytcs3YdGaJoL3p667l2aKBSl5IWK5TH4V8EQc5jc5'; // ✋ استبدل ده بتوكنك
+  final String repoOwner = 'Mahmoud-Gharib';
   final String repoName = 'app_upload';
   final String repoFolder = 'image'; // اسم المجلد داخل الريبو
 
